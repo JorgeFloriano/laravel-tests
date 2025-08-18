9f3719870c3cabe2849c710a060e95b3959d099c
Jorge Luis <jorgefloriano1646@gmail.com>
2025-08-18 16:43:05 -0300

    Class 07.3

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -4,20 +4,40 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Product;
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProductTest extends TestCase
 {
+    use RefreshDatabase;
+
+    //protected $seed = true;
     public function test_if_product_inserted_is_instance_of_Model(): void
     {
-        $product = Product::factory()->create();
+        $product = Product::factory()->create([
+            'name' => 'product 01',
+            'description' => 'Product 01 Description',
+            'price' => 100,
+        ]);
 
-        $this->assertInstanceOf(Product::class, actual:$product);
+        $this->assertDatabaseHas('products', [
+            'name' => 'product 01',
+        ]);
+
+        $this->assertInstanceOf(Product::class, actual: $product);
     }
 
     public function test_inserted_10_products(): void
     {
         Product::factory()->count(10)->create();
 
-        $this->assertEquals(expected:10, actual:Product::count());
+        //$this->seed();
+
+        //$this->assertEquals(expected:10, actual:Product::count());
+
+        //$this->assertDatabaseEmpty(table:'products');
+
+        $this->assertDatabaseCount(table:'products', count:10);
+
     }
 }
